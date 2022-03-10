8695d0196e5b143229eac74a61403d6157792eeb
Ahaan18 <71545644+Ahaan18@users.noreply.github.com>
2022-03-10 18:46:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("welcome!");
+                console.log("website created by ahaan jain from 6d");
+                console.log("if you are seeing this then the website has loaded and will function properly");
+                function mamba(){
+                    console.log("transferring");
+                    window.location = "mamba.html";
+                }
+                function tiger(){
+                    console.log("transferring");
+                    window.location = "tiger.html";
+                }
+                function source(){
+                    console.log("transferring");
+                    window.location = "sources.html";
+                }
+function back(){
+    console.log("transferring");
+    window.location = "index.html";
+}
